e23fb083a54f5a2cd638e06a3667a22644aa37d6
Gerson Ep <gerson21millones@gmail.com>
2023-03-09 09:41:37 -0600

    4-Instalacion de React y react DOM + Estructuramiento de la App

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+
+  //rafce --importa react y crea el componente //imr importa react
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1>Hola mundo</h1>
+  );
+}
+
+export default App
+
+
+
+
+
