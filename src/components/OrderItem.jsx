2797592cd682f7a7509a9550a1c551cba9662eee
Gerson Ep <gerson21millones@gmail.com>
2023-03-12 22:24:57 -0600

    17-React.useState. Ejemplo funcionamiento del HOOK useState en ProductItem

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react'; 
 /* import '../styles/OrderItem.scss'; */
 /* import IconClose from '../asset/icons/icon_close.png'; */
 
@@ -6,9 +6,10 @@ import '@styles/OrderItem.scss';
 import IconClose from '@icons/icon_close.png';
 
 const OrderItem = () => {
+
   return (
 <div className="OrderItem">
-			<figure>
+			<figure> 
 				<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
 			</figure>
 			<p>Bike</p>
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react'; //useState es el HOOK
 import '@styles/ProductItem.scss';
 import BTaddToCard from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+
+		//trabajar el estado dentro de React usando HOOK
+//const 2 valores 1er es el estado el 2 es una funcion modificadora del 1 elemento del estado, y en useState se pasa el valor inicial de cart(1) puede ir un string '', true, 0, o un objeto {}, o arreglo [] 
+const [cart, setCart] = useState('hola');// '' reciba un string. - un arreglo para listar multiples elementos que van a estar dentro de la estructura.
+
+//trabajar con el valor de set: modificar el html para que al darle clic desencadene el llamdo 
+const handleClick = () => { //esta funcion modifica el estado, es llamada desde el html figure
+	setCart('Hola Mundo');
+}
+
   return (
 <div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -11,9 +21,10 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleClick}> {/* onClick para escuchar el clic, llame a la funcion handleClick */}
 				<img src={BTaddToCard} alt="" />
 				</figure>
+				{cart} {/* llamada de cart */}
 			</div>
 		</div>
   )
