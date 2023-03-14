054b82dc34f61e551b761ed75f3d93db678614c1
Gerson Ep <gerson21millones@gmail.com>
2023-03-14 16:35:46 -0600

    25-Usando Reduce_Calculando el precio total. En MyOrder y OrderItem, agregar productos al carrito se van sumando los precios y mustra la imagen del producto seleccionado. ok

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -8,6 +8,12 @@ const MyOrder = () => {
 
 	const {state:{cart}} = useContext(AppContext);
 
+	//valor reduce: logica de acumular y agregar los valores que necesitamos
+	const sumTotal = () => {
+		const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+		const sum = cart.reduce(reducer, 0); //suma de la totalidad del elemento, se le pasa el reducer
+		return sum;
+	}
 
   return (
     <aside className="MyOrder">
@@ -26,7 +32,7 @@ const MyOrder = () => {
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>${sumTotal()}</p>{/* <p>$560.00</p> */}  {/**Cada vez q se agrega un Item se suma y pasa a sumTotal */}
 				</div>
 				<button className="primary-button">
 					Checkout
